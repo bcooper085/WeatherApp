0005190106a343feea04013cb67684fb4ae379b1
Brandon Cooper and Sandro Mijatovic <brandon+sandro@>
2017-05-11 08:59:33 -0700

    add weather temp to return object

--- wwwroot/js/weather-interface.js
+++ wwwroot/js/weather-interface.js
@@ -8,8 +8,9 @@
             data: {cityName: $("#cityName").val()},
             dataType: 'json',
             success: function (result) {
-                var htmlString;
-                $(".weather").html(result.name);
+                var kelvin = parseInt(result.main.temp);
+                var fahrenheit = Math.round(((9 / 5) * (kelvin - 273)) + 32);
+                $(".weather").html(fahrenheit);
                 console.log(result);
             }
         })
